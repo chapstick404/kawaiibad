e3f6147d8b7c4e2d558662863f95f01cabe4f340
chapstick404 <null.nullth@gmail.com>
2020-08-19 21:03:29 -0600

    added new commands

--- kawaiibad/src/kawaiibad.py
+++ kawaiibad/src/kawaiibad.py
@@ -17,7 +17,7 @@ async def on_ready():
 @bot.command()
 async def help(ctx): ##help text to a dm
 	usr =ctx.author
-	await usr.send("```Ping			pong\nbotserver			my home is here\nneko			NEKO NEKO NEE!\nf			put a f in the chat\ncookie			someone wants cookies```\n")
+	await usr.send("```Ping			pong\nbotserver			my home is here\nneko			NEKO NEKO NEE!\nf			put a f in the chat\ncookie			someone wants cookies\npat			someone really wants a pat\nkiss			give someone a nice kiss\nhug 				give a hug to someone in need```")
 
 @bot.command() ##good debug tool
 async def ping(ctx):
@@ -30,7 +30,7 @@ async def botserver(ctx):
 @bot.command()
 async def neko(ctx): ##pulls random neko from site that the og used
 	message = nekos.img('neko')
-	await ctx.send('```' + message '```')
+	await ctx.send(message)
 
 @bot.command()
 async def f(ctx, arg):
@@ -39,6 +39,24 @@ async def f(ctx, arg):
 @bot.command()
 async def cookie(ctx, arg):
 	await ctx.send('**' + str(arg) + '**, you got a :cookie: from **' + str(ctx.author) + '**')
+
+@bot.command()
+async def baka(ctx):
+	message = nekos.img('baka')
+	await ctx.send(message)
+@bot.command()
+async def pat(ctx, arg):
+	message = "**" + str(arg) + "** you got pat by **" + str(ctx.author) + '**\n' + str(nekos.img('pat'))
+	await ctx.send(message)
+
+@bot.command()
+async def kiss(ctx, arg):
+	message = "**" + str(arg) + '** you got kissed by **' + str(ctx.author) + '**\n' + str(nekos.img('kiss'))
+	await ctx.send(message)
+@bot.command()
+async def hug(ctx, arg):
+	message = '**' + str(arg) + '** you got hugged by **' + str(ctx.author) + '**\n' + str(nekos.img('hug'))
+	await ctx.send(message)
 
 bot.run(token)
 
