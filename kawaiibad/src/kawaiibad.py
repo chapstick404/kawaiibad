05765fec6583b3fe0150005753cf75a6ebc182ee
chapstick404 <null.nullth@gmail.com>
2020-08-19 18:18:49 -0600

    added logic to try to remove 404 list of 404 numbers

--- kawaiibad/src/kawaiibad.py
+++ kawaiibad/src/kawaiibad.py
@@ -1,9 +1,20 @@
 import discord
 import os
+import random
+import urllib
 
 from discord.ext import commands
 token = os.environ['TOKEN']
 
+neko404 = open("neko404.txt", "r")
+listneko404 = neko404.read().splitlines()
+
+non404 = []
+for i in range(100,299):
+	if i in listneko404:
+		continue
+	else:
+		non404.append(i)
 
 bot = commands.Bot(command_prefix = '+&')
 @bot.remove_command('help')
@@ -11,8 +22,6 @@ bot = commands.Bot(command_prefix = '+&')
 @bot.event
 async def on_ready():
 	await bot.change_presence(activity=discord.Game(name="+& help"))
-	print('logged in as')
-	print(bot.user.name)
 @bot.command()
 async def help(ctx):
 	usr =ctx.author
@@ -25,6 +34,13 @@ async def ping(ctx):
 @bot.command()
 async def botserver(ctx):
 	await ctx.send('do you really think I have a sever? bruh')
+
+@bot.command()
+async def neko(ctx):
+
+	message = ('https://cdn.nekos.life/neko/neko_' + str(non404[random.randint(0,len(non404))]) + '.png')
+	await ctx.send(message)
+	await ctx.send('if this command returned a website with a 404 error please contact chapstick404#7405')
 
 bot.run(token)
 
